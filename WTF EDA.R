3bc42aa3ec87ac1c3089210f1b7e5f3ad10e6795
couyang24 <30837746+couyang24@users.noreply.github.com>
2018-08-22 14:08:13 -0400

    update

--- WTF EDA.R
+++ WTF EDA.R
@@ -88,6 +88,65 @@ glimpse(key)
 
 sum(is.na(train))/(ncol(train)*nrow(train))
 
+tdates <- train %>% select(-Page)
+
+
+
+foo <- train %>% select(Page) %>% rownames_to_column()
+
+mediawiki <- foo %>% filter(str_detect(Page, "mediawiki"))
+
+wikimedia <- foo %>% filter(str_detect(Page, "wikimedia"))
+
+wikipedia <- foo %>% filter(str_detect(Page, "wikipedia")) %>% 
+  
+  filter(!str_detect(Page, "wikimedia")) %>%
+  
+  filter(!str_detect(Page, "mediawiki"))
+
+
+
+wikipedia <- wikipedia %>%
+  
+  separate(Page, into = c("foo", "bar"), sep = ".wikipedia.org_") %>%
+  
+  separate(foo, into = c("article", "locale"), sep = -3) %>%
+  
+  separate(bar, into = c("access", "agent"), sep = "_") %>%
+  
+  mutate(locale = str_sub(locale,2,3))
+
+
+
+wikimedia <- wikimedia %>%
+  
+  separate(Page, into = c("article", "bar"), sep = "_commons.wikimedia.org_") %>%
+  
+  separate(bar, into = c("access", "agent"), sep = "_") %>%
+  
+  add_column(locale = "wikmed")
+
+
+
+mediawiki <- mediawiki %>%
+  
+  separate(Page, into = c("article", "bar"), sep = "_www.mediawiki.org_") %>%
+  
+  separate(bar, into = c("access", "agent"), sep = "_") %>%
+  
+  add_column(locale = "medwik")
+
+
+
+tpages <- wikipedia %>%
+  
+  full_join(wikimedia, by = c("rowname", "article", "locale", "access", "agent")) %>%
+  
+  full_join(mediawiki, by = c("rowname", "article", "locale", "access", "agent"))
+
+
+
+sample_n(tpages, size = 5)
 
 
 
